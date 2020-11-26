3ea1497ad0133fc3f48e1a878f99b70ae345ce34
sapir <s>
2020-11-26 17:25:38 +0200

    Buildstone2 (ex3): No.2 O(n) algo, but not working..

--- minCircle.h
+++ minCircle.h
@@ -3,10 +3,12 @@
 #ifndef MINCIRCLE_H_
 #define MINCIRCLE_H_
 
+#include <array>
 #include <iostream>
-#include <vector>
+#include <iterator>
 #include <limits>
-
+#include <random>
+#include <vector>
 
 
 using namespace std;
@@ -30,124 +32,192 @@ public:
 
 ///function that return the distance between two points
 float dist(const Point &a, const Point &b) {
-    return ((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    return std::hypot(a.x - b.x, a.y - b.y);
 }
+
+const double EPSILON = 1 + 1e-14;
 
 /// function that checks if the point inside or on the boundaries of the circle
 bool ifInsideCircle(const Circle &c, const Point &point) {
-    return dist(c.center, point) <= c.radius;
+    return dist(c.center, point) <= c.radius * EPSILON;
 }
 
-//// function to get a circle defined by 3 points
-Point getCircleCenter(float ax, float ay, float bx, float by) {
-    float A = ax * ax + ay * ay;
-    float B = bx * bx + by * by;
-    float C = ax * by - ay * bx;
-    return {(by * B - ay * C) / (2 * C), (ax * C - bx * B) / (2 * C)};
-}
+// todo explain
+Circle circleFromThree(const Point &a, const Point &b, const Point &c) {
 
-/// Function to return a circle that intersects three points
-Circle circleFromThree(const Point &A, const Point &B, const Point &C) {
-    Point c = getCircleCenter(B.x - A.x, B.y - A.y, C.x - A.x, C.y - A.y);
-    c.x += A.x;
-    c.y += A.y;
-    return {C, dist(C, A)};
-}
+        float x1 = a.x;
+        float y1 = a.y;
 
-/// Function to return a circle that intersects two points
-Circle circleFromTwo(const Point &A, const Point &B) {
-    // Set the center to be the midpoint of A and B
-    Point C = {(A.x + B.x) / 2, (A.y + B.y) / 2};
+        float x2 = b.x;
+        float y2 = b.y;
 
-    // Set the radius to be half the distance AB
-    return {C, dist(A, B) / 2};
-}
+        float x3 = c.x;
+        float y3 = c.y;
 
 
-/// Function to check if the given circle encloses the given points (that in the vector)
-bool ifCircleEnclose(const Circle &c, Point **points, size_t size) {
-    // passing all the points and check ig it inside the circle of not
-    for (int i = 0; i < size; ++i) {
-        if (!ifInsideCircle(c, *points[i])) return false;
+        float x12 = x1 - x2;
+        float x13 = x1 - x3;
+
+        float y12 = y1 - y2;
+        float y13 = y1 - y3;
+
+        float y31 = y3 - y1;
+        float y21 = y2 - y1;
+
+        float x31 = x3 - x1;
+        float x21 = x2 - x1;
+
+        // x1^2 - x3^2
+        float sx13 = pow(x1, 2) - pow(x3, 2);
+
+        //y1^2 - y3^2
+        float sy13 = pow(y1, 2) - pow(y3, 2);
+
+        float sx21 = pow(x2, 2) - pow(x1, 2);
+        float sy21 = pow(y2, 2) - pow(y1, 2);
+
+        float f = ((sx13) * (x12)
+                 + (sy13) * (x12)
+                 + (sx21) * (x13)
+                 + (sy21) * (x13))
+                / (2 * ((y31) * (x12) - (y21) * (x13)));
+        float g = ((sx13) * (y12)
+                 + (sy13) * (y12)
+                 + (sx21) * (y13)
+                 + (sy21) * (y13))
+                / (2 * ((x31) * (y12) - (x21) * (y13)));
+
+        float c1 = -pow(x1, 2) - pow(y1, 2) - 2 * g * x1 - 2 * f * y1;
+
+        // eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+        // where centre is (h = -g, k = -f) and radius r
+        // as r^2 = h^2 + k^2 - c
+        float h = -g;
+        float k = -f;
+        float sqr_of_r = h * h + k * k - c1;
+
+        // r is the radius
+        float r = sqrt(sqr_of_r);
+        return {Point(h, k), r};
+
     }
-    return true;
+//    double ox = (min(min(a.x, b.x), c.x) + max(max(a.x, b.x), c.x)) / 2;
+//    double oy = (min(min(a.y, b.y), c.y) + max(max(a.y, b.y), c.y)) / 2;
+//    double ax = a.x - ox, ay = a.y - oy;
+//    double bx = b.x - ox, by = b.y - oy;
+//    double cx = c.x - ox, cy = c.y - oy;
+//    double d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2;
+//    if (d == 0) return {Point(0, 0), 0};
+//    double x = ((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) +
+//                (cx * cx + cy * cy) * (ay - by)) /
+//               d;
+//    double y = ((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) +
+//                (cx * cx + cy * cy) * (bx - ax)) /
+//               d;
+//    Point p{(float) (ox + x), (float) (oy + y)};
+//    float r = max(max(dist(p, a), dist(p, b)), dist(p, c));
+//    return Circle{p, r};
+//}
+
+Point subtractTwoPoints(Point point1, Point point2) {
+    return {point1.x - point2.x, point1.y - point2.y};
 }
+
+float vecMult(Point point1, Point point2) { return point1.x * point2.y - point2.x * point1.y; }
+
+///function that calculates the circle of two points while the points is the Diameter of the circle
+Circle twoPointsDiameter(const Point &point1, const Point &point2) {
+    Point center{(point1.x + point2.x) / 2.0f, (point1.y + point2.y) / 2.0f};
+    return Circle{center, max(dist(point1, center), dist(point2, center))};
+}
+
+Circle minCircleFromTwo(const Point &point1, const Point &point2, size_t size, Point **points) {
+    // initialize left circle
+    Circle leftCircle = Circle(Point(0, 0), -1);
+    // initialize right circle
+    Circle rightCircle = Circle(Point(0, 0), -1);
+    // initialize circle from two points
+    Circle circleFromTwoPoints = twoPointsDiameter(point1, point2);
+    // calculate point that created from subtraction of two points
+    Point subp1p2 = subtractTwoPoints(point2, point1);
+    //Circle circleFromThreePoints = {{0, 0}, -1};
+    // for each point in the array, I will check if it inside the circle
+    for (int i = 0; i < size; ++i) {
+        const Point point3 = *points[i];
+        if (ifInsideCircle(circleFromTwoPoints, point3)) { continue; }
+        // calculate point that created from subtraction of two points
+        Point subp3p1 = subtractTwoPoints(point3, point1);
+        // calculate the vector multiplication
+        //cross
+        double vecMultipication = vecMult(subp1p2, subp3p1);
+        // calculate the circle that made from three points
+        Circle circleFromThreePoints = circleFromThree(point1, point2, point3);
+        // if the circle was just initialized
+        if (circleFromThreePoints.radius < 0) continue;
+        // decide which circle is right circle and which is left
+        else if (vecMultipication > 0.0f &&
+                 ((leftCircle.radius < 0.0f ||
+                   vecMult(subp1p2, subtractTwoPoints(circleFromThreePoints.center, point1)) >
+                           vecMult(subp1p2, subtractTwoPoints(leftCircle.center, point1))))) {
+            leftCircle = circleFromThreePoints;
+        } else if (vecMultipication < 0.0f &&
+                   ((rightCircle.radius < 0 ||
+                     vecMult(subp1p2, subtractTwoPoints(circleFromThreePoints.center, point1)) <
+                             vecMult(subp1p2, subtractTwoPoints(rightCircle.center, point1))))) {
+            rightCircle = circleFromThreePoints;
+        }
+
+        // now, I'm comparing between the circles (according to the minimal radius).
+        if (leftCircle.radius < 0 && rightCircle.radius < 0) return circleFromTwoPoints;
+        // if the left circle is illegal
+        else if (leftCircle.radius < 0) {
+            return rightCircle;
+        }
+        // if the right circle is illegal
+        else if (rightCircle.radius < 0) {
+            return leftCircle;
+        } else {
+            // if they both legal, return this one with the minimal radius
+            return leftCircle.radius <= rightCircle.radius ? leftCircle : rightCircle;
+        }
+    }
+}
+
+Circle minCircleFromOne(Point point1, size_t size, Point **points) {
+    //        Circle circle = twoPointsDiameter(point1, *points[0]);
+    Circle circle = {point1, 0};
+    for (int i = 0; i < size; ++i) {
+        Point point2 = *points[i];
+        // check if the circle that I initialized already enclose the current point
+        if (!ifInsideCircle(circle, point2)) {
+            if (circle.radius == 0) {
+                circle = twoPointsDiameter(point1, point2);
+            } else {
+                circle = minCircleFromTwo(point1, point2, i + 1, points);
+            }
+        }
+    }
+    return circle;
+}
+
+#include <algorithm>
 
 /// function that return the minimum circle of the given points
 Circle findMinCircle(Point **points, size_t size) {
+    //    if (size == 0) { return {Point(0, 0), 0}; }
+    //    if (size == 1) { return {*points[0], 0}; }
 
-    if (size == 0) { return Circle(Point(0, 0), 0); }
-    if (size == 1) {
-        return Circle(*points[0], 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(&points[0], &points[size], g);
+
+    Circle tmp = Circle(Point(0, 0), -1);
+    for (int j = 0; j < size; ++j) {
+        Point p = *points[j];
+        if (tmp.radius < 0 || !ifInsideCircle(tmp, p)) { tmp = minCircleFromOne(p, j + 1, points); }
     }
-
-    // Circle from the two first points for start
-    Circle C2 = Circle(Point((points[0]->x + points[1]->x) / 2,
-                                 (points[0]->y + points[1]->y) / 2),
-                           float(dist(*points[0], *points[1]) / 2));
-
-    Circle tmp = Circle(Point(0, 0), 0);
-    // for all the points, i will check if the point is inside the circle that already calculated in the previous iteration or not.
-    // if yes, I will use it.
-    for (int i = 3; i < size; ++i) {
-        Circle prevC = Circle(Point((points[i-1]->x + points[i-2]->x) / 2,
-                                (points[i-1]->y + points[i-2]->y) / 2),
-                          float(dist(*points[i-1], *points[i-2]) / 2));
-        if (ifInsideCircle(prevC, *points[i])) {
-            tmp = prevC;
-        } else {
-            //solution with one point
-            // for each point from j=2 until size check if it inside the circle
-            // if not, solution with two points - with line...
-        };
-
-    }
-
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////
-
-//    //const int infinity = std::numeric_limits<int>::max();
-//
-//    // Set initial MEC to have infinity radius
-//    Circle mec = { { 0, 0 }, 429496725.0};
-//
-//    // Go over all pair of points
-//    Circle tmp = {{0, 0}, 0};
-//    for (int i = 0; i < size; i++) {
-//        for (int j = i + 1; j < size; j++) {
-//
-//            // Get the smallest circle that
-//            // intersects P[i] and P[j]
-//            tmp = circleFromtwo(*points[i], *points[j]);
-//
-//            // Update MEC if tmp encloses all points
-//            // and has a smaller radius
-//
-//            if (tmp.radius < mec.radius && ifCircleEnclose(tmp, points, size))
-//                mec = tmp;
-//        }
-//    }
-//
-//    // Go over all triples of points
-//    for (int i = 0; i < size; i++) {
-//        for (int j = i + 1; j < size; j++) {
-//            for (int k = j + 1; k < size; k++) {
-//
-//                // Get the circle that intersects P[i], P[j], P[k]
-//                Circle tmp = circleFromThree(*points[i], *points[j], *points[k]);
-//
-//                // Update MEC if tmp encloses all points
-//                // and has smaller radius
-//                if (tmp.radius < mec.radius && ifCircleEnclose(tmp, points, size))
-//                    mec = tmp;
-//            }
-//        }
-//    }
-//    return mec;
+    return tmp;
 }
-
 
 
 #endif /* MINCIRCLE_H_ */
---
+++ minCircle2.h
@@ -0,0 +1,152 @@
+
+#ifndef MINCIRCLE2_H_
+#define MINCIRCLE2_H_
+
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <random>
+#include <vector>
+
+
+using namespace std;
+
+// ------------ DO NOT CHANGE -----------
+class Point {
+public:
+    float x, y;
+    Point(float x, float y) : x(x), y(y) {}
+};
+
+class Circle {
+public:
+    Point center;
+    float radius;
+    Circle(Point c, float r) : center(c), radius(r) {}
+};
+// --------------------------------------
+
+// you may add helper functions here
+
+///function that return the distance between two points
+float dist(const Point &a, const Point &b) { return std::hypot(a.x - b.x, a.y - b.y); }
+
+const double EPSILON = 1 + 1e-14;
+
+/// function that checks if the point inside or on the boundaries of the circle
+bool ifInsideCircle(const Circle &c, const Point &point) {
+    return dist(c.center, point) <= c.radius * EPSILON;
+}
+
+Point get_circle_center(float bx, float by, float cx, float cy) {
+    float B = bx * bx + by * by;
+    float C = cx * cx + cy * cy;
+    float D = bx * cy - by * cx;
+    return {(cy * B - by * C) / (2 * D), (bx * C - cx * B) / (2 * D)};
+}
+
+
+// Function to return a unique circle that
+// intersects three points
+Circle circle_from(const Point &A, const Point &B, const Point &C) {
+    Point I = get_circle_center(B.x - A.x, B.y - A.y, C.x - A.x, C.y - A.y);
+
+    I.x += A.x;
+    I.y += A.y;
+    return {I, dist(I, A)};
+}
+
+// Function to return the smallest circle
+// that intersects 2 points
+Circle circle_from(const Point &A, const Point &B) {
+    // Set the center to be the midpoint of A and B
+    Point C = {(A.x + B.x) / 2.0f, (A.y + B.y) / 2.0f};
+
+    // Set the radius to be half the distance AB
+    return {C, dist(A, B) / 2.0f};
+}
+
+// Function to check whether a circle
+// encloses the given points
+bool is_valid_circle(const Circle &c, const vector<const Point *> &P) {
+    // Iterating through all the points
+    // to check  whether the points
+    // lie inside the circle or not
+    for (auto p : P)
+        if (!ifInsideCircle(c, *p)) return false;
+    return true;
+}
+
+// Function to return the minimum enclosing
+// circle for N <= 3
+Circle min_circle_trivial(std::vector<const Point *> P) {
+    //assert(P.size() <= 3);
+    if (P.empty()) {
+        return {{0, 0}, 0};
+    } else if (P.size() == 1) {
+        return {*P[0], 0};
+    } else if (P.size() == 2) {
+        return circle_from(*P[0], *P[1]);
+    }
+
+    // To check if MEC can be determined
+    // by 2 points only
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            Circle c = circle_from(*P[i], *P[j]);
+            if (is_valid_circle(c, P)) return c;
+        }
+    }
+    return circle_from(*P[0], *P[1], *P[2]);
+}
+
+// Returns the MEC using Welzl's algorithm
+// Takes a set of input points P and a set R
+// points on the circle boundary.
+// n represents the number of points in P
+// that are not yet processed.
+Circle welzl_helper(std::vector<const Point *> P, std::vector<const Point *> R, size_t n) {
+    // Base case when all points processed or |R| = 3
+    if (n == 0 || R.size() == 3) { return min_circle_trivial(R); }
+
+    // Pick a random point randomly
+    int idx = rand() % n;
+    const Point *p = P[idx];
+
+    // Put the picked point at the end of P
+    // since it's more efficient than
+    // deleting from the middle of the vector
+    swap(P[idx], P[n - 1]);
+
+    // Get the MEC circle d from the
+    // set of points P - {p}
+    Circle d = welzl_helper(P, R, n - 1);
+
+    // If d contains p, return d
+    if (ifInsideCircle(d, *p)) { return d; }
+
+    // Otherwise, must be on the boundary of the MEC
+    R.push_back(p);
+
+    // Return the MEC for P - {p} and R U {p}
+    return welzl_helper(P, R, n - 1);
+}
+
+#include <algorithm>
+
+Circle findMinCircle(Point **points, size_t size) {
+    vector<const Point *> P_copy;
+    P_copy.reserve(size);
+    for (int i = 0; i < size; i++) { P_copy.push_back(points[i]); }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(P_copy.begin(), P_copy.end(), g);
+
+    std::vector<const Point *> R;
+    return welzl_helper(P_copy, R, P_copy.size());
+}
+
+// Driver code
+#endif//MINCIRCLE2_H_
